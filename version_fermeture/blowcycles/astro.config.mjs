8bdb5c0b8f2efa6ab55dd234f6f689476d7bc8a4
Babouh <sonny.lion@laposte.net>
2024-10-26 15:20:46 +0200

    Add the data

--- version_fermeture/blowcycles/astro.config.mjs
+++ version_fermeture/blowcycles/astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
-
+import yaml from '@rollup/plugin-yaml';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  vite: {
+    plugins: [yaml()]
+  }
 });
